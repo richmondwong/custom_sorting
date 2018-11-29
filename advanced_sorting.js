609c755b60f8d97fbc025a9dbd3e9c0983932748
Richmond Wong <richmondwong@gmail.com>
2018-11-29 18:24:57 +0000

    Created sort function

---
+++ advanced_sorting.js
@@ -0,0 +1,36 @@
+
+var students = [
+ { id: 1, name: "bruce",  age: 40 },
+ { id: 2, name: "zoidberg", age: 22 },
+ { id: 3, name: "alex",   age: 22 },
+ { id: 4, name: "alex",   age: 30 }
+];
+​
+​
+​
+var sortAge=function(a,b){
+ return b.age -a.age;
+}
+​
+var sortName = function(a,b){
+ var nameA=a.name;
+ var nameB=b.name;
+ console.log(nameA,nameB)
+​
+ if (nameA < nameB){
+  console.log('nameA comes before nameB')
+  return -1;
+}
+else if (nameA > nameB){
+ console.log('nameB comes before nameA')
+  return 1;
+ }
+ else {
+  console.log('nameA === nameB')
+  return sortAge(a,b)
+ }
+}
+​
+​
+​
+console.log(students.sort(sortName))
---
+++ array_sort.js
@@ -0,0 +1,7 @@
+var arr = [10, 2, 5, 1, 9];
+arr.sort(function(a, b) {
+  return a - b;
+});
+console.log(numbers);
+
+// [1, 2, 3, 4, 5]
